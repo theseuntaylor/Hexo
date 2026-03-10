84a333907a6db24273c5f63145212354ad89278c
theseuntaylor <seun.gtaylor@gmail.com>
2026-03-10 00:06:47 +0000

    Update project configuration and cleanup MainActivity
    - Upgrade Java SDK and bytecode target level from JDK 21 to JDK 24.
    - Remove unused `Greeting` composable and its preview from `MainActivity`.
    - Add `.idea/deviceManager.xml` to `.gitignore`.
    - Update IntelliJ IDEA project configuration files.

--- app/src/main/java/com/theseuntaylor/hexo/MainActivity.kt
+++ app/src/main/java/com/theseuntaylor/hexo/MainActivity.kt
@@ -61,19 +61,3 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    HexoTheme {
-        Greeting("Android")
-    }
-}
